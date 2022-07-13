ce81f509967154dcd01f8afafefcb7d09b60744c
gourvankit <gourvankit23@gmail.com>
2022-07-13 10:42:17 +0530

    Second

--- api/Routes/lists.js
+++ api/Routes/lists.js
@@ -44,34 +44,31 @@ router.get("/", verify, async (req, res) => {
   const typeQuery = req.query.type;
   const genreQuery = req.query.genre;
   let foundList = [];
-  if (req.user.isAdmin) {
-    try {
-      if (typeQuery) {
-        if (genreQuery) {
-          foundList = await List.aggregate([
-            { $match: { type: typeQuery, genre: genreQuery } },
-            { $sample: { size: 10 } },
-          ]);
-        } else {
-          foundList = await List.aggregate([
-            { $match: { type: typeQuery } },
-            { $sample: { size: 10 } },
-          ]);
-        }
+
+  try {
+    if (typeQuery) {
+      if (genreQuery) {
+        foundList = await List.aggregate([
+          { $match: { type: typeQuery, genre: genreQuery } },
+          { $sample: { size: 10 } },
+        ]);
       } else {
         foundList = await List.aggregate([
-          {
-            $sample: { size: 10 },
-          },
+          { $match: { type: typeQuery } },
+          { $sample: { size: 10 } },
         ]);
       }
-
-      res.status(201).json(foundList);
-    } catch (err) {
-      res.status(500).json(err);
+    } else {
+      foundList = await List.aggregate([
+        {
+          $sample: { size: 10 },
+        },
+      ]);
     }
-  } else {
-    res.status(403).json("You are not allowed!");
+
+    res.status(201).json(foundList);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
